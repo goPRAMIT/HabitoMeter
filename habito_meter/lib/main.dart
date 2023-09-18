e968c183f5b21d9de238d7477c0d966325837dfc
Pramit <75903538+goPRAMIT@users.noreply.github.com>
2023-09-18 23:56:14 +0530

    Minor UI changes

--- habito_meter/lib/main.dart
+++ habito_meter/lib/main.dart
@@ -4,17 +4,25 @@ import 'package:habito_meter/onboarding_screen.dart';
 import 'package:habito_meter/pages/home_page.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 //import 'package:google_fonts/google_fonts.dart';
 
 int? isViewed;
 
 Future<void> main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   await Hive.initFlutter();
   await Hive.openBox("Habit_Database");
-  WidgetsFlutterBinding.ensureInitialized();
+  //WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences prefs = await SharedPreferences.getInstance();
   isViewed = prefs.getInt('onboarding_screen');
+
   runApp(const MyApp());
+
+  await Future.delayed(const Duration(milliseconds: 1000));
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
--- habito_meter/lib/splash_screen.dart
+++ habito_meter/lib/splash_screen.dart
@@ -1,80 +1,80 @@
-//import 'dart:async';
+// //import 'dart:async';
 
-import 'dart:async';
+// import 'dart:async';
 
-import 'package:flutter/material.dart';
-import 'package:habito_meter/onboarding_screen.dart';
-import 'package:lottie/lottie.dart';
-//import 'package:video_player/video_player.dart';
+// import 'package:flutter/material.dart';
+// import 'package:habito_meter/onboarding_screen.dart';
+// //import 'package:lottie/lottie.dart';
+// //import 'package:video_player/video_player.dart';
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+// class SplashScreen extends StatefulWidget {
+//   const SplashScreen({super.key});
 
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
+//   @override
+//   State<SplashScreen> createState() => _SplashScreenState();
+// }
 
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
 
-    Timer(Duration(milliseconds: 5000), () {
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
-        return OnBoardingScreen();
-      }));
-    });
-  }
+//     Timer(Duration(milliseconds: 0), () {
+//       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+//         return OnBoardingScreen();
+//       }));
+//     });
+//   }
 
-  //late VideoPlayerController _controller;
+//   //late VideoPlayerController _controller;
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  // _controller = VideoPlayerController.asset('assets/habitoMeter.mp4')
-  //   ..initialize().then((value) {
-  //     setState(() {});
-  //   })
-  //   ..setVolume(0.0);
-  // _playVideo();
-  //}
+//   // @override
+//   // void initState() {
+//   //   super.initState();
+//   // _controller = VideoPlayerController.asset('assets/habitoMeter.mp4')
+//   //   ..initialize().then((value) {
+//   //     setState(() {});
+//   //   })
+//   //   ..setVolume(0.0);
+//   // _playVideo();
+//   //}
 
-  // void _playVideo() async {
-  //   _controller.play();
+//   // void _playVideo() async {
+//   //   _controller.play();
 
-  //   await Future.delayed(const Duration(seconds: 2));
-  //   Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
-  //     return OnBoardingScreen();
-  //   }));
-  // }
+//   //   await Future.delayed(const Duration(seconds: 2));
+//   //   Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+//   //     return OnBoardingScreen();
+//   //   }));
+//   // }
 
-  // @override
-  // void dispose() {
-  //   _controller.dispose();
-  //   super.dispose();
-  // }
+//   // @override
+//   // void dispose() {
+//   //   _controller.dispose();
+//   //   super.dispose();
+//   // }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Container(
-        //color: Colors.black,
-        child: Center(
-          child: Lottie.asset('animations/finalLogo.json', repeat: false),
-        ),
-      ),
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.black,
+//       // body: Container(
+//       //   //color: Colors.black,
+//       //   child: Center(
+//       //     child: Lottie.asset('animations/1.json', repeat: false),
+//       //   ),
+//       // ),
 
-      // backgroundColor: Colors.black,
-      // body: Center(
-      //   child: _controller.value.isInitialized
-      //       ? AspectRatio(
-      //           aspectRatio: _controller.value.aspectRatio,
-      //           child: VideoPlayer(
-      //             _controller,
-      //           ))
-      //       : Container(),
-      // ),
-    );
-  }
-}
+//       // backgroundColor: Colors.black,
+//       // body: Center(
+//       //   child: _controller.value.isInitialized
+//       //       ? AspectRatio(
+//       //           aspectRatio: _controller.value.aspectRatio,
+//       //           child: VideoPlayer(
+//       //             _controller,
+//       //           ))
+//       //       : Container(),
+//       // ),
+//     );
+//   }
+// }
